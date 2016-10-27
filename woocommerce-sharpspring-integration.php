94fe30145d4106e6a464d0957ca3cea54a707b24
Bob Bolender <bob.bolender@fahrenheit.io>
2016-10-27 11:23:39 -0500

    Add order_metabox for responses from SharpSpring

--- woocommerce-sharpspring-integration.php
+++ woocommerce-sharpspring-integration.php
@@ -42,6 +42,7 @@ class WC_SS_Plugin {
     }
     add_action('woocommerce_order_action_send_lead_to_sharpspring', array( $this,'send_lead_to_sharpspring'));
     add_filter('woocommerce_order_actions',  array( $this,'order_actions'), 10, 1);
+    add_action('add_meta_boxes', array( $this,'order_metabox'));
   }
 
   public function send_lead_to_sharpspring($order) {
@@ -57,6 +58,26 @@ class WC_SS_Plugin {
   public function order_actions($actions) {
     $actions['send_lead_to_sharpspring'] = __("Send lead to SharpSpring", 'woocommerce-sharpspring');
     return $actions;
+  }
+
+  public function order_metabox() {
+    add_meta_box('ss-box', 'SharpSpring Data', array( $this, 'order_metabox_output' ), 'shop_order', 'normal', 'low');
+  }
+
+  public function order_metabox_output($order) {
+    $data = get_post_meta($order->ID);
+    echo "<dl>";
+    if (!empty($data)) foreach ($data as $key => $values) {
+      if (strpos($key, '_ss') === 0) {
+        foreach ($values as $value) {
+          if (isset($value)){
+            echo "<dt>$key</dt>";
+            echo '<dd><pre style="max-height: 100px; overflow-y: scroll; padding: 0.3em; border: 1px solid #ccc;">'.$value.'</pre><a href="#" style="float: right; font-size: 12px;" onclick="var event = arguments[0] || window.event; event.preventDefault(); jQuery(this).hide().parent(\'dd\').find(\'pre\').css(\'max-height\', \'none\');">expand</a><br style="clear:all"></dd>';
+          }
+        }
+      }
+    }
+    echo "</dl>";
   }
 }
 
