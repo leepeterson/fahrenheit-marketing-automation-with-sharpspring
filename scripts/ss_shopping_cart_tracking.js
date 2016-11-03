a1c7b1b9a3cb0e49e0051f95afed5275b68d1305
Bob Bolender <bob.bolender@fahrenheit.io>
2016-11-03 15:24:27 -0500

    Don't use global scope

--- scripts/ss_shopping_cart_tracking.js
+++ scripts/ss_shopping_cart_tracking.js
@@ -1,45 +1,40 @@
-var data              = ss_shopping_cart_tracking_data;
-var transaction_data  = ss_shopping_cart_tracking_data.transaction_data;
+(function(){
+  var data              = ss_shopping_cart_tracking_data;
+  var transaction_data  = ss_shopping_cart_tracking_data.transaction_data;
 
-var _ss = _ss || [];
+  var _ss = _ss || [];
 
-var debug = 0;
-if (debug) console.log(data);
+  var debug = 0;
+  if (debug) console.log(data);
 
-function addItems(){
-  // Set recently removed items to quantity 0
-  var removed = data.removed_cart_contents;
-  for (var key in removed){
-    if (removed.hasOwnProperty(key)){
-      var item = removed[key];
-      item.quantity = 0;
-      if (debug) console.log(item);
-      addItem(item);
+  function addItems(){
+    // Set recently removed items to quantity 0
+    var removed = data.removed_cart_contents;
+    for (var key in removed){
+      if (removed.hasOwnProperty(key)){
+        var item = removed[key];
+        item.quantity = 0;
+        if (debug) console.log(item);
+        _ss.push(['_addTransactionItem', item ]);
+      }
+    }
+
+    // Add cart contents to transaction
+    // Items that have been removed and re-added will appear in both lists!
+    var added = data.cart_contents;
+    for (var key in added){
+      if (added.hasOwnProperty(key)){
+        var item = added[key];
+        if (debug) console.log(item);
+        _ss.push(['_addTransactionItem', item ]);
+      }
+    }
+
+    if (data.orderComplete){
+      _ss.push(['_completeTransaction', { 'transactionID': data.transaction_data.transactionID } ]);
     }
   }
 
-  // Add cart contents to transaction
-  // Items that have been removed and re-added will appear in both lists!
-  var added = data.cart_contents;
-  for (var key in added){
-    if (added.hasOwnProperty(key)){
-      var item = added[key];
-      if (debug) console.log(item);
-      addItem(item);
-    }
-  }
-}
-
-function addItem(item){
-  _ss.push(['_addTransactionItem', {
-    'transactionID':    transaction_data.transactionID,
-    'itemCode':         item.code,
-    'productName':      item.name,
-    'category':         item.category,
-    'price':            item.price,
-    'quantity':         item.quantity
-  }]);
-}
-
-_ss.push(['_setTransaction', transaction_data, addItems ]);
+  _ss.push(['_setTransaction', transaction_data, addItems ]);
+})();
 
