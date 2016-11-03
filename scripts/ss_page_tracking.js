45282c6e3b967ca9c81971f21842ddc7ffcc8ca1
Bob Bolender <bob.bolender@fahrenheit.io>
2016-11-03 13:26:17 -0500

    Fix sharpspring tracking script URL

--- scripts/ss_page_tracking.js
+++ scripts/ss_page_tracking.js
@@ -1,13 +1,15 @@
 var _ss = _ss || [];
-console.log(ss_account_settings);
 _ss.push(['_setDomain', ss_account_settings.domain]);
 _ss.push(['_setAccount', ss_account_settings.account]);
 _ss.push(['_trackPageView']);
+
+var subDomain = ss_account_settings.domain.match(/https?:\/\/([^.]+)./)[1];
+
 (function() {
     var ss = document.createElement('script');
     ss.type = 'text/javascript'; ss.async = true;
 
-    ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + ss_account_settings.account + '.marketingautomation.services/client/ss.js?ver=1.1.1';
+    ss.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + subDomain + '.marketingautomation.services/client/ss.js?ver=1.1.1';
     var scr = document.getElementsByTagName('script')[0];
     scr.parentNode.insertBefore(ss, scr);
 })();
